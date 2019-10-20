3e32e43f3820bb8f9a85c551d84342241adf774a
daisyarusey <daisyarusey.dc@gmail.com>
2019-10-20 21:56:02 +0300

    create AuthListener

--- app/src/main/java/com/moringaschool/gymsmart/SignUp_Activity.java
+++ app/src/main/java/com/moringaschool/gymsmart/SignUp_Activity.java
@@ -17,12 +17,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUp_Activity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = SignUp_Activity.class.getSimpleName();
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private FirebaseAuth mAuth;
     @BindView(R.id.createUserButton) Button mCreateUserButton;
@@ -38,15 +41,31 @@ public class SignUp_Activity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_);
 
-        ButterKnife.bind(this);
 
         ButterKnife.bind(this);
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
     }
 
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(SignUp_Activity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
 
 
     @Override
