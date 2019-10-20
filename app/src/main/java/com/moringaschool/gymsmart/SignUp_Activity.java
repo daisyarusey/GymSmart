b34154a2660bbeade8a36fa2861588854405029b
daisyarusey <daisyarusey.dc@gmail.com>
2019-10-20 11:40:22 +0300

    Configure google sign in

--- app/src/main/java/com/moringaschool/gymsmart/SignUp_Activity.java
+++ app/src/main/java/com/moringaschool/gymsmart/SignUp_Activity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp_Activity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
+    // Configure Google Sign In
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
