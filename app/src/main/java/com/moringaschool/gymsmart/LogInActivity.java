352dc9f49620e23003d060996022cc5aea8dfe87
daisyarusey <daisyarusey.dc@gmail.com>
2019-10-22 17:11:05 +0300

    Add method login with password

--- app/src/main/java/com/moringaschool/gymsmart/LogInActivity.java
+++ app/src/main/java/com/moringaschool/gymsmart/LogInActivity.java
@@ -1,14 +1,20 @@
 package com.moringaschool.gymsmart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
@@ -16,6 +22,7 @@ import butterknife.ButterKnife;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String TAG = LogInActivity.class.getSimpleName();
     @BindView(R.id.registerTextView) TextView mRegisterTextView;
     @BindView(R.id.passwordLoginButton) Button mPasswordLoginButton;
     @BindView(R.id.emailEditText) EditText mEmailEditText;
@@ -39,6 +46,25 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LogInActivity.this, SignUp_Activity.class);
             startActivity(intent);
             finish();
+        }else {
+            loginWithPassword();
         }
     }
+
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
+
+
+    }
+
+
 }
