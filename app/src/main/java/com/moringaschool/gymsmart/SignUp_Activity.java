4a8f4bbf655cc5844c75339cdf58f98e3a08b550
daisyarusey <daisyarusey.dc@gmail.com>
2019-10-28 18:05:45 +0300

    Open gallery

--- app/src/main/java/com/moringaschool/gymsmart/SignUp_Activity.java
+++ app/src/main/java/com/moringaschool/gymsmart/SignUp_Activity.java
@@ -1,11 +1,17 @@
 package com.moringaschool.gymsmart;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -18,10 +24,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +42,7 @@ public class SignUp_Activity extends AppCompatActivity implements View.OnClickLi
     static  int REQUESCODE = 1;
     static  int PReqCode = 1;
     Uri pickedImgUrl;
+
     public static final String TAG = SignUp_Activity.class.getSimpleName();
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -139,7 +151,11 @@ public class SignUp_Activity extends AppCompatActivity implements View.OnClickLi
             startActivity(intent);
             finish();
         }else if (v==profile){
-            takPictureIntent();
+            if (Build.VERSION.SDK_INT>=22){
+                checkAndRequestPermission();
+            }else{
+                openGallery();
+            }
         }
         else {
             final String name = mNameEditText.getText().toString().trim();
@@ -160,13 +176,27 @@ public class SignUp_Activity extends AppCompatActivity implements View.OnClickLi
             }
         }
 
-    private void takPictureIntent() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
+    private void checkAndRequestPermission() {
+        if (ContextCompat.checkSelfPermission(SignUp_Activity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            !=PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(SignUp_Activity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Toast.makeText(SignUp_Activity.this,"Plaese",Toast.LENGTH_SHORT).show();
+            }else {
+                ActivityCompat.requestPermissions(SignUp_Activity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PReqCode);
+            }
 
+        }else {
+            openGallery();
+        }
 
     }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }
+
 
 
     private void createNewUser(String name,String email,String password) {
@@ -184,8 +214,6 @@ public class SignUp_Activity extends AppCompatActivity implements View.OnClickLi
                             Log.d(TAG, "Authentication successful");
 
                             updateUserInfo(name,pickedImgUrl,mAuth.getCurrentUser());
-                            Intent intent = new Intent(SignUp_Activity.this,MainActivity.class);
-                            startActivity(intent);
                             mAuthProgressDialog.dismiss();
                         } else {
                             Toast.makeText(SignUp_Activity.this, "Authentication failed.",
@@ -199,6 +227,47 @@ public class SignUp_Activity extends AppCompatActivity implements View.OnClickLi
     }
 
     private void updateUserInfo(String name, Uri pickedImgUrl, FirebaseUser currentUser) {
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        final StorageReference imageFilePath = mStorage.child(pickedImgUrl.getLastPathSegment());
+        imageFilePath.putFile(pickedImgUrl).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .setPhotoUri(uri)
+                                .build();
 
+                        currentUser.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            Log.d(TAG, "Regesteration complete");
+                                            Intent intent = new Intent(SignUp_Activity.this,MainActivity.class);
+                                            startActivity(intent);
+
+                                        }
+                                    }
+                                });
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode==RESULT_OK && requestCode== REQUESCODE && data != null){
+            pickedImgUrl= data.getData();
+            profile.setImageURI(pickedImgUrl);
+
+        }
     }
 }
